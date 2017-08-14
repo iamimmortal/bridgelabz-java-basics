bb6acf1533b1ddd8810939be1126c339d466258c
Dharmendra mourya <drmourya5@gmail.com>
2017-08-14 20:24:45 +0530

    update 2.0

--- com/profound/util/RollDie.java
+++ com/profound/util/RollDie.java
@@ -6,22 +6,27 @@
 *****************************************/
 package com.profound.util;
 
+/*Math class involves the inclusion of math fuctions*/
 import java.lang.Math;
 import java.util.Scanner;
 
 public class RollDie 
-{
-    int result,trials;
-        //counter for each number
-        int one=0,two=0,three=0,four=0,five=0,six=0;
+{.       
+         //variables to store result and number of trails from user
+         static int result,trials;
+         //counter for each number
+         static int one,two,three,four,five,six;
 
 
     public static void main(String[] args) 
-    {
+    {.   
+        //Initialization
+        one=0,two=0,three=0,four=0,five=0,six=0;
         Scanner in=new Scanner(System.in);
         trials=in.nextInt();
         while(trials>0)
         {
+         //Provide a number between 1 to 6 each times it call
         result=(int)((Math.random()*6)+1);
         if(result==1)
         one++;
@@ -35,13 +40,15 @@ public class RollDie
         five++;
         else
         six++;
+         //After every trail it must be decremented
         trials--;
         }
         //largest falling side
         int array[]={one,two,three,four,five,six};
         int greatest=array[0];
         for(int i=0;i<5;i++)
-        {
+        {.   
+             //This logic find the greatest
             if(array[i]>greatest)
             {
                 greatest=array[i];
