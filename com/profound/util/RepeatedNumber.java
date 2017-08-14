af3037a9e2a43bdf7f1e76f97a24dbe493eef17a
Dharmendra mourya <drmourya5@gmail.com>
2017-08-14 20:15:38 +0530

    update 2.0

--- com/profound/util/RepeatedNumber.java
+++ com/profound/util/RepeatedNumber.java
@@ -11,32 +11,34 @@ import java.util.Scanner;
 
 public class RepeatedNumber 
 {  
-    //loop control variable
+     //loop control variable
      static int i,j;
-
+     //variable to store the occurrence of repeated number
+     static int counter;
     public static void main(String[] args) 
     {
-
-        int array[]=new int[10];
+        //Array to store 100 numbers
+        int array[]=new int[100];
         Scanner in=new Scanner(System.in);
         for(i=0;i<=99;i++)
         {
             array[i]=in.nextInt();
-            
         }
         for(i=1;i<=100;i++)
-        { int c=0;
+        { 
+            counter=0;
             for(j=0;j<=99;j++)
             {
                 if(i==array[j])
                 {
-                   c++;
+                   counter++;
                 }
             }
-            if(c>1)
+            //If number is repeated then counter will be greater than one
+            if(counter>1)
             {
             
-            System.out.println("The Number:"+i+"repeated "+c+"times");
+            System.out.println("The Number:"+i+"repeated "+counter+"times");
             System.out.println();
             }
         }
