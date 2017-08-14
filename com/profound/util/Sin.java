675c13ead59286036837215b91f6345727e48e64
Dharmendra mourya <drmourya5@gmail.com>
2017-08-14 20:40:35 +0530

    Updated 2.0

--- com/profound/util/Sin.java
+++ com/profound/util/Sin.java
@@ -11,36 +11,37 @@ import java.util.Scanner;
 
 public class Sin 
 {
+        //variable to control lenght of series
+        static int lenghtOfSeries;
+        //get an angle from user 
+        static double x;
+        //variable computes power of x
+        static int p=1;
+        //assign sign + or -
+        static int sign=1;
+        //calculate sum of series
+        static double sum=0.0;
+        //for loop control
+        static int i;
 
     public static void main(String[] args) 
     {
-        //variable to control lenght of series
-        int lenghtOfSeries;
-        //get an angle from user 
-        double x;
-        //variable computes power of x
-        int p=1;
-        //assign sign + or -
-        int sign=1;
-        //calculate sum of series
-        double sum=0.0;
-        //for loop
-        int i;
+        
         //scan inputs
         Scanner in=new Scanner(System.in);
         System.out.println("Enter lenght of taylor series:");
         lenghtOfSeries=in.nextInt();
         System.out.println("Enter angle:");
         x=in.nextDouble();
-        //convert x to be between -2PI to 2PI
+        //convert x to be between -2PI to 2PI for precision
         x=x%(2*Math.PI);
         for(i=1;i<=lenghtOfSeries;i++)
-        {
+        {.  //Here fact method is called
             sum+=sign*(Math.pow(x,p)/fact(p));
             sign=-sign;
             p+=2;
         }
-       // System.out.println("Sinx:"+sum);
+        //System.out.println("Sinx:"+sum);
         System.out.println("Sinx="+Math.sin(sum));
     }
     //calculate factorial
