3040a4fab6b1126392c4d2ea219a55f054f89ae2
Dharmendra mourya <drmourya5@gmail.com>
2017-08-14 20:08:59 +0530

    update

--- com/profound/util/PrimeNumberInRange.java
+++ com/profound/util/PrimeNumberInRange.java
@@ -1,19 +1,22 @@
-
+/*************************************
+    Program:prime number according to user define range
+    Author: Dharmendra Mourya
+    Date:13/08/2017
+**************************************/
 package com.profound.util;
 
 import java.util.Scanner;
 
-
-import java.util.Scanner;
-
 public class PrimeNumberInRange 
-{
+{      
+        //user provided range
+        static int range;
+        //loop control 
+        static int c=2,i,j;
 
     public static void main(String[] args) 
     {
-        int range;
-        //loop control 
-        int c=2,i,j;
+        
         Scanner in=new Scanner(System.in);
         System.out.println("Enter range for prime number:");
         range=in.nextInt();
@@ -21,14 +24,17 @@ public class PrimeNumberInRange
         {
             for(j=2;j<=c-1;j++)
             {
+                //if divide then break loop 
                 if(c%j==0)
                    break;
             }
                 if(j==c)
                 {
                     System.out.println(c);
+                    //i is incremented only if number divide by itself
                     i++;
                 }
+            //number increment
             c++;
             }
             
