db4a07d057a20f26e893c62e25f7ddb0443ee690
Dharmendra mourya <drmourya5@gmail.com>
2017-08-15 13:24:19 +0530

    version 2.0

---
+++ com/profound/util/PrintInitials.java
@@ -0,0 +1,23 @@
+/*************************************
+    Program:Print initials K D W
+    Author: Dharmendra Mourya
+    Date:14/08/2017
+**************************************/
+package com.profound;
+
+public class PrintInitials 
+{
+
+    public static void main(String[] args) 
+    {
+        System.out.println("**    ***  *********       **             *             **");
+        System.out.println("**   ***   **       **      **           ***           **");
+        System.out.println("**  ***    **        **      **         ** **         **");
+        System.out.println("** ***     **         **      **       **   **       **");
+        System.out.println("*****      **         **       **     **     **     **");
+        System.out.println("** ***     **         **        **   **       **   **");
+        System.out.println("**  ***    **        **          ** **         ** **");
+        System.out.println("**   ***   **       **            ***           ***");
+        System.out.println("**    ***  *********               *             *");
+    }
+}
