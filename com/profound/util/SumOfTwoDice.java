d72ac104cbcfa1e10021904e0401601ab9076e40
Dharmendra mourya <drmourya5@gmail.com>
2017-08-15 17:55:07 +0530

    Update SumOfTwoDice.java

--- com/profound/util/SumOfTwoDice.java
+++ com/profound/util/SumOfTwoDice.java
@@ -1,14 +1,18 @@
-package com.profound.util;
+/**********************************
+Program: sum of two dice
+Author: Dharmendra mourya
+Date:12/08/17
+************************************/
+packagepackage com.profound.util;
 
 import java.lang.Math;
 
 public class SumOfTwoDice 
 {
-
+    //Variable to store two dice result
+     static double d1,d2;
     public static void main(String[] args) 
     {
-        double d1,d2;
-        
         System.out.println("Rolling dice 1...");
         d1=(int)(Math.random()*6+1);
         System.out.println("Rolling dice 2...");
