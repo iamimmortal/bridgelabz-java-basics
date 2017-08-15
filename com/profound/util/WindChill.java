ffa14612047c1b4995d070faa70088f2ea043cfc
Dharmendra mourya <drmourya5@gmail.com>
2017-08-15 17:45:13 +0530

    java-15/08/17

--- com/profound/util/WindChill.java
+++ com/profound/util/WindChill.java
@@ -1,15 +1,24 @@
-package com.profound.util;
+/****************************************
+   program:based on wind velocity count windchill
+   Authur: Dharmendra mourya
+   Version:1.0
+   Date:11/08/17
+  ******************************************/
+packagepackage com.profound.util;
 
+//Math class for inclusion of math built in functions
 import java.lang.Math;
 import java.util.Scanner;
 
 public class WindChill 
 {
-
+       //variables store temperature,wind velocity by user
+       static float tempreture,velocity;
+       //Temp variable
+       static float temp;
     public static void main(String[] args) 
-    {   //2 variables
-        float tempreture,velocity,temp;
-        //scanner class
+    {  
+        //scanner class object 
         Scanner in=new Scanner(System.in);
         //taking value of tempreture
         System.out.println("Enter tempreture in fareinheit:");
@@ -22,7 +31,6 @@ public class WindChill
         //variable to store wind chil
         double windChill=(float)(35.74)+(0.6215*tempreture)+(0.4275*tempreture-35.75)*temp;
         //display result
-        
         System.out.println("Windchill:"+windChill);
     }
 }
